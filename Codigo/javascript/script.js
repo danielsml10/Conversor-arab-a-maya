20f742b6062be0a5dc59b26ccccfe3b3dc1a2b66
Daniel Murillo <dsml.cubs@gmail.com>
2021-01-24 02:54:56 -0700

    version3

--- Codigo/javascript/script.js
+++ Codigo/javascript/script.js
@@ -4,30 +4,33 @@
  var suma = 0;
 
 function convNum(num){
-	let n = parseInt(num.value);
-	let x,y,z;
-	let nivel1 = document.getElementById('nivel1');
-	let nivel2 = document.getElementById('nivel2');
-	let nivel3 = document.getElementById('nivel3');
-		if(n<20){
-			x=n;
-			y=null;
-			z=null;
-		}else if(n>=20 && n<400){
-			y=Math.trunc(n/20);
-			x = n-(y*20);
-			z=null;
-		}else{
-			z = Math.trunc(n/400);
-			y = Math.trunc((n-(z*400))/20);
-			x = n-(z*400)-(y*20);
-		}
-		console.log("el primer nivel es igual a: "+x);
-		nivel1.src ="imagenes/"+x+".png";
-		console.log("el segundo nivel es igual a: "+y);
-		if(y==null) nivel2.src = ""; else nivel2.src = "imagenes/"+y+".png";
-		console.log("el tercer nivel es igual a: "+z);
-		if(z==null) nivel3.src=""; else nivel3.src = "imagenes/"+z+".png";
+	if(parseInt(num.value) >= 0 && parseInt(num.value) < 8000 ){
+		let n = parseInt(num.value);
+		let x,y,z;
+		let nivel1 = document.getElementById('nivel1');
+		let nivel2 = document.getElementById('nivel2');
+		let nivel3 = document.getElementById('nivel3');
+			if(n<20){
+				x=n;
+				y=null;
+				z=null;
+			}else if(n>=20 && n<400){
+				y=Math.trunc(n/20);
+				x = n-(y*20);
+				z=null;
+			}else{
+				z = Math.trunc(n/400);
+				y = Math.trunc((n-(z*400))/20);
+				x = n-(z*400)-(y*20);
+			}
+			console.log("el primer nivel es igual a: "+x);
+			nivel1.src ="imagenes/"+x+".png";
+			console.log("el segundo nivel es igual a: "+y);
+			if(y==null) nivel2.src = ""; else nivel2.src = "imagenes/"+y+".png";
+			console.log("el tercer nivel es igual a: "+z);
+			if(z==null) nivel3.src=""; else nivel3.src = "imagenes/"+z+".png";
+
+	}else alert('Solo se permiten valores entre 0 y 7999');
 }
 
 function seleccion(img){
@@ -63,9 +66,9 @@ function enviar(){
 }
 
 function limpiar(){
-	document.getElementById('nivel1m').src='';
-	document.getElementById('nivel2m').src='';
-	document.getElementById('nivel3m').src='';
+	document.getElementById('nivel1m').src='imagenes/nivel.png';
+	document.getElementById('nivel2m').src='imagenes/nivel.png';
+	document.getElementById('nivel3m').src='imagenes/nivel.png';
 	document.getElementById('btnLimpiar').disabled=true;
 	document.getElementById('numConv').value='';
 	suma = 0;
